305dea71de3274f9ba399e86062f7124f2541501
Nagesh Mishra <nagesh3.13@gmail.com>
2020-06-25 23:52:15 +0530

    Add files via upload

---
+++ precache-manifest.c1b68415107c1f7dabdaae1bfa0b6f22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2638cc73f5eb590ddef867cb59771e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e969773eeb38d3122071",
+    "url": "./static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "68114e7db1d01a2898eb",
+    "url": "./static/css/main.c44c802a.chunk.css"
+  },
+  {
+    "revision": "e969773eeb38d3122071",
+    "url": "./static/js/2.b992cd90.chunk.js"
+  },
+  {
+    "revision": "d0b30a1909a753c393e134caf98fa9bb",
+    "url": "./static/js/2.b992cd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68114e7db1d01a2898eb",
+    "url": "./static/js/main.c0a97e19.chunk.js"
+  },
+  {
+    "revision": "2f7361b376f7c0af02df",
+    "url": "./static/js/runtime-main.bd0fb421.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lush-interior/precache-manifest.2414462c6769495a6a019bf77699caad.js"
+  "./precache-manifest.c1b68415107c1f7dabdaae1bfa0b6f22.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lush-interior/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

5a29a237210aec317f94c43c9927709296d201cf
Nagesh Mishra <nagesh3.13@gmail.com>
2020-06-25 18:45:19 +0530

    Add files via upload

---
+++ precache-manifest.2c469ab19d57b0f13bab99c58cae10a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ae8804db794767ebaa85bb3da16464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48af2eb2e55b4bcd8f8d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "45c8306d7a74f49faf77",
+    "url": "/static/js/2.cd3de57c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cd3de57c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48af2eb2e55b4bcd8f8d",
+    "url": "/static/js/main.ad8d05f6.chunk.js"
+  },
+  {
+    "revision": "81d85e473862d9ebf136",
+    "url": "/static/js/runtime-main.dede5d52.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
